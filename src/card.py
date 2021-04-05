bc832c2044799533dc42f7c0cf36e008c3fa7dc1
IOIOB <70326335+IOIOB@users.noreply.github.com>
2021-04-05 22:09:31 +0200

    Add str method

--- src/card.py
+++ src/card.py
@@ -13,3 +13,14 @@ class Card:
     def __iter__(self):
         return iter(
             [value for key, value in self.__dict__.items() ])
+
+    def __str__(self):
+        return (
+            f"Card:\n"
+            f"  id:\n\t{self.id}\n"
+            f"  question:\n \t{self.question}\n"
+            f"  answer:\n\t{self.answer}\n"
+            f"  colour:\n\t{self.colour}\n"
+            f"  image:\n\t{self.image}\n"
+            f"  streak:\n\t{self.streak}\n"
+            f"  sessionAsked:\n\t{self.sessionAsked}\n")
