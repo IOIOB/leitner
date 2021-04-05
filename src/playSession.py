05895b236123e4cffc90f260c1835b24a9e1d9c9
IOIOB <70326335+IOIOB@users.noreply.github.com>
2021-04-05 22:08:25 +0200

    Add playSession functionality

--- src/playSession.py
+++ src/playSession.py
@@ -1,4 +1,13 @@
-import cards
+import card
 
 def play(session, cards):
-    return (session, cards)
+    newCards = []
+    for card in cards:
+        print(str(card))
+        guess = input("Got it?")
+        if guess.lower().startswith("y"):
+            card.streak = card.streak + 1
+            card.sessionAsked = session
+            newCards.insert(card)
+    newSession = session + 1
+    return (newSession, newCards)
