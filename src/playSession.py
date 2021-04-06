5a2d4505f628388b9ee307cd1be63e8ff36e7847
IOIOB <70326335+IOIOB@users.noreply.github.com>
2021-04-06 09:58:18 +0200

    Add card promotion/demotion

--- src/playSession.py
+++ src/playSession.py
@@ -2,12 +2,16 @@ import card
 
 def play(session, cards):
     newCards = []
+    print(f"Session: {session}")
     for card in cards:
         print(str(card))
         guess = input("Got it?")
         if guess.lower().startswith("y"):
             card.streak = card.streak + 1
             card.sessionAsked = session
-            newCards.insert(card)
+        else:
+            card.streak = 1
+            card.sessionAsked = session
+        newCards.insert(0, card)
     newSession = session + 1
     return (newSession, newCards)
