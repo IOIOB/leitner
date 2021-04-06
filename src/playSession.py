6086fe2b9cdb622a4cdfb234ace943a9ddafc885
IOIOB <70326335+IOIOB@users.noreply.github.com>
2021-04-06 13:12:16 +0200

    Prettify, extract getAnswerInput function

--- src/playSession.py
+++ src/playSession.py
@@ -1,17 +1,45 @@
 import card
 
+def getAnswerInput():
+    print("Did you get it?")
+    print("('y' if you did, 'n' if you didn't): ", end="")
+    rawInput = input()
+
+    if rawInput.lower() == "y":
+        return True
+    elif rawInput.lower() == "n":
+        return False
+    else:
+        print(f"{rawInput} is not a valid answer...")
+        return getAnswerInput()
+
 def play(session, cards):
     newCards = []
-    print(f"Session: {session}")
-    for card in cards:
-        print(str(card))
-        guess = input("Got it?")
-        if guess.lower().startswith("y"):
+
+    print(f"Current session: {session}")
+    print("")
+    
+    for i, card in enumerate(cards):
+        print("")
+        print(f"Question {i}:")
+        print(f"  {card.question}")
+        print("(Press Enter to show answer)")
+
+        input()
+
+        print("Answer:")
+        print(f"  {card.answer}")
+        print("")
+
+        isAnswerCorrect = getAnswerInput()
+        
+        if isAnswerCorrect:
             card.streak = card.streak + 1
             card.sessionAsked = session
         else:
             card.streak = 1
             card.sessionAsked = session
         newCards.insert(0, card)
+
     newSession = session + 1
     return (newSession, newCards)
